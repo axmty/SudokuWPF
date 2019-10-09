b4ed33cd51744e3d5525bf977ac4e30208ae640f
Metgy Alexandre <alexandre.metgy@gmail.com>
2019-10-09 23:48:50 +0200

    Add SudokuGrid class.

---
+++ SudokuWPF/SudokuGrid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SudokuWPF
+{
+    public class SudokuGrid
+    {
+        private readonly int[,] _grid = new int[9, 9];
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            if (!(obj is SudokuGrid otherGrid))
+            {
+                throw new ArgumentException(nameof(obj));
+            }
+            else
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (_grid[i, j] != otherGrid._grid[i, j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this._grid);
+        }
+    }
+}
